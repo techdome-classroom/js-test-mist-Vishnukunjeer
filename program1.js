119b93106c31cc541e80ba05498abe343901dbc8
Vishnukunjeer <101510914+Vishnukunjeer@users.noreply.github.com>
2024-02-22 13:44:54 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,11 +1,36 @@
 /**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
-    
+* @param {string} s
+* @return {boolean}
+*/
+var isValid = function(s){
+   const stack = [];
+   const validBrackets = {
+       '(': ')',
+       '{': '}',
+       '[': ']',
+   };
+
+   for (const char of s) {
+       if (validBrackets[char]) {
+           stack.push(char);
+       } else {
+           const lastBracket = stack.pop();
+           if (validBrackets[lastBracket] !== char) {
+               return false;
+           }
+       }
+   }
+
+   return stack.length === 0;
 };
 
+
+console.log(isValid("()")); 
+console.log(isValid("()[]{}")); 
+console.log(isValid("(]")); 
+
+
 module.exports = { isValid };
+
 
 
